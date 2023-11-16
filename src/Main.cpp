7593e57c97b5c10444917faaa3bfd136e76d1dc1
Aditya Jain <adityajain.aj10@gmail.com>
2023-11-15 19:20:00 -0600

    add logger

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,10 @@
 #include <argparse/argparse.hpp>
+#include "WowLogger.H"
+
 #include <iostream>
 
 int main() {
   std::cout << "hello world" << std::endl;
+  LogInfo("Testing Logger");
   return 0;
 }

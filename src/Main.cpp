1f98c8c8e0e6e53cd675753dd784d7265a3d7f39
Aditya Jain <adityajain.aj10@gmail.com>
2023-11-22 23:10:10 -0600

    parser fixups

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,31 @@
+#include <argparse/argparse.hpp>
 #include "parser.hpp"
 
-int main(int argc, char *argv[]) {
-  if(argc!=3) {
-    std::cout<<"Wrong input format. Correct Format: [INPUT_FILE] [OUTPUT_FILE]\n";
-    return 0;
+int main( int argc, char** argv ) {
+  argparse::ArgumentParser program( "main" );
+  program.add_argument( "--input" )
+         .required()
+         .help( "input file to read from, provided by ISPD 2009" );
+
+  program.add_argument( "--output" )
+         .required()
+         .help( "file to write output to" );
+
+  try {
+    program.parse_args( argc, argv );
+  } catch ( const std::runtime_error& err ) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    std::exit(1); 
   }
 
-  struct inparams t = parse(argv[1]);
+  auto inputFile = program.get<std::string>( "--input" );
+  auto outputFile = program.get<std::string>( "--output" );
 
-  //DME here
+  auto inp = parse( inputFile );
 
-  struct outparams t_out; 
-  //print_output(argv[2], t_out);
+  outparams test;
+  print_output(outputFile, test);
 
   return 0;
 }

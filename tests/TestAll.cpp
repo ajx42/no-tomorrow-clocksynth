6b152eeabcc16cb7c4b279a619cf84819fc1cfe3
Aditya Jain <adityajain.aj10@gmail.com>
2023-12-04 23:43:37 -0600

    first cut dme

--- tests/TestAll.cpp
+++ tests/TestAll.cpp
@@ -65,3 +65,56 @@ TEST_CASE( "DME::DMETiledRegion Intersection Test", "[dme]" ) {
 
   REQUIRE( intersection.value() == coreR );
 }
+
+
+TEST_CASE( "DME::DMETiledRegion Intersection Test 0 Radius Segment Core", "[dme]" ) {
+  auto core1 = dme::DMECore {
+    .Kind = dme::DMECore::SEGMENT,
+    .Loc = dme::seg_t {{.x = 0, .y = 0}, {.x = 5, .y = 5}}
+  };
+
+  auto reg1 = dme::DMETiledRegion( core1, 0 );
+
+  auto core2 = dme::DMECore {
+    .Kind = dme::DMECore::SEGMENT,
+    .Loc = dme::seg_t {{.x = 5, .y = 0}, {.x = 15, .y = 10}}
+  };
+
+  auto reg2 = dme::DMETiledRegion( core2, 5 );
+  
+  auto intersection = dme::getTRRIntersection(reg1, reg2);
+  REQUIRE( intersection.has_value() );
+
+  auto coreR = dme::DMECore {
+    .Kind = dme::DMECore::SEGMENT,
+    .Loc = dme::seg_t {{.x = 0, .y = 0}, {.x = 5, .y = 5}}
+  };
+
+  REQUIRE( intersection.value() == coreR );
+}
+
+TEST_CASE( "DME::DMETiledRegion Intersection Test 0 Radius Point Core", "[dme]" ) {
+  auto core1 = dme::DMECore {
+    .Kind = dme::DMECore::POINT,
+    .Loc = dme::pt_t {.x = 0, .y = 0}
+  };
+
+  auto reg1 = dme::DMETiledRegion( core1, 0 );
+
+  auto core2 = dme::DMECore {
+    .Kind = dme::DMECore::SEGMENT,
+    .Loc = dme::seg_t {{.x = 5, .y = 0}, {.x = 15, .y = 10}}
+  };
+
+  auto reg2 = dme::DMETiledRegion( core2, 5 );
+  
+  auto intersection = dme::getTRRIntersection(reg1, reg2);
+  REQUIRE( intersection.has_value() );
+
+  auto coreR = dme::DMECore {
+    .Kind = dme::DMECore::POINT,
+    .Loc = dme::pt_t{.x = 0, .y = 0},
+  };
+
+  REQUIRE( intersection.value() == coreR );
+}
